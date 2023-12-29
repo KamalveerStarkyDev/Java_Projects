be246940710a198fed2882126de656a68a044b15
KamalveerStarkyDev <140949464+KamalveerStarkyDev@users.noreply.github.com>
2023-12-29 12:28:40 +0530

    wrote full code for number guessing~

--- number_guessing_game/number_guessing.java
+++ number_guessing_game/number_guessing.java
@@ -1,11 +1,39 @@
-// creating a java package
-package guessing_game;
+import java.util.Random;
+import java.util.Scanner;
 
-// importing swing package
-import java.swing.*;
-
-public class GuessingGame{
-    public static void main(String[] args){
+public class number_guessing {
+    public static void main(String[] args) {
         
+        // Generate a random number between 1 and 100
+        Random random = new Random();
+        int targetNumber = random.nextInt(100) + 1;
+
+        // Initialize variables
+        int userGuess;
+        int attempts = 0;
+
+        // Create Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between 1 and 100. Try to guess it.");
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess < targetNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > targetNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+
+        } while (userGuess != targetNumber);
+
+        // Close the Scanner
+        scanner.close();
     }
 }
